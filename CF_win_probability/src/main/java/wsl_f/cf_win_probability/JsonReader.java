6f4dbe1e9280aa2217580ef1c73aad39a2878b52
Wsl_F <WasylFranchuk@gmail.com>
2016-05-09 11:22:19 +0300

    add Json Reader

---
+++ CF_win_probability/src/main/java/wsl_f/cf_win_probability/JsonReader.java
@@ -0,0 +1,42 @@
+package wsl_f.cf_win_probability;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * copied from
+ * https://github.com/nestor-by/map-api-samples/blob/master/src/main/java/by/googlemapid/sample/JsonReader.java
+ *
+ */
+public class JsonReader {
+
+    private static String readAll(final Reader rd) throws IOException {
+        final StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject read(final String url) throws IOException, JSONException {
+        final InputStream is = new URL(url).openStream();
+        try {
+            final BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            final String jsonText = readAll(rd);
+            final JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+}
